a933e8dcf96aa635ab38dbf6b5dea6047e8ff21f
Matthew Boston <matthew.boston@gmail.com>
2015-05-13 20:59:56 -0600

    use environ to get the $PORT from heroku

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [ring/ring-jetty-adapter "1.3.2"]
                  [hiccup "1.0.5"]
                  [postmark "1.1.0" :exclusions [org.clojure/clojure]]
+                 [environ "1.0.0"]
 
                  ;; client
                  [org.clojure/clojurescript "0.0-3269"]
--- src/clj/clojurelive_web/web.clj
+++ src/clj/clojurelive_web/web.clj
@@ -1,8 +1,9 @@
 (ns clojurelive-web.web
   (:require [clojurelive-web.handler :as handler]
             [compojure.handler :as compojure]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
 (defn -main [& [port]]
-  (let [port (Integer. (or port 5000))]
+  (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (compojure/site #'handler/app) {:port port :join? false})))
